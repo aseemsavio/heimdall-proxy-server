763a8199829d58ba22cdb52a183fda5835c3911d
Aseem Savio <aseemsavio3@gmail.com>
2019-04-19 19:50:24 +0530

    Update HeimdallApplication.java

--- src/main/java/com/savio/heimdall/HeimdallApplication.java
+++ src/main/java/com/savio/heimdall/HeimdallApplication.java
@@ -2,11 +2,13 @@ package com.savio.heimdall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class HeimdallApplication {
 
     public static void main(String[] args) {

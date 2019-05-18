14d966ee586e1eb188c8f4850544be618b2091d0
Aseem Savio <aseemsavio3@gmail.com>
2019-05-18 11:37:32 +0530

    Update HeimdallController.java

--- src/main/java/com/savio/heimdall/controller/HeimdallController.java
+++ src/main/java/com/savio/heimdall/controller/HeimdallController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class HeimdallController {
         //List<AwakeResponse> awakeResponseList = heimdallService.wakeUpCall();
 
 
-        List<AwakeResponse> awakeResponseList = null;
+        List<AwakeResponse> awakeResponseList = new ArrayList<>();
         awakeResponseList.add(new AwakeResponse("Aseem", "awake"));
         awakeResponseList.add(new AwakeResponse("Savio", "sleeping"));
         return ResponseEntity.ok().header("status", "success").body(awakeResponseList);
